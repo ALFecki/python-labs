bd910c6fe992e2cce57b503c98987d8ead8740e0
Skvortsov A.V <alexskvr03@gmail.com>
2023-02-16 22:26:41 +0300

    hardcode deleted + added user input + gitignore update

--- lab-1/main.py
+++ lab-1/main.py
@@ -4,10 +4,20 @@ from utilities import arithmetic_operation
 def main():
     print("Hello, World!")
     
-    a = arithmetic_operation(5, 10, "add")
-    print("Result is ", a)
+    print("Please enter first number: ")
+    first = int(input)
+
+    print("Please enter second number: ")
+    second = int(input)
+
+    print("Please enter operation: ")
+    operation = str(input)
+    
+    res = arithmetic_operation(first, second, operation)
+    print("Result is ", res)
 
     
+
     
 
 
--- lab-1/utilities.py
+++ lab-1/utilities.py
@@ -1,13 +1,15 @@
 
+import os
+
 def arithmetic_operation(first, second, operation):
     match operation:
-        case "add":
+        case os.environ.get('ADD'):
             return first + second
-        case "sub":
+        case os.environ.get('SUB'):
             return first - second
-        case "mul":
+        case os.environ.get('MUL'):
             return first * second
-        case "div":
+        case os.environ.get('DIV'):
             return first / second
         case _:
             print("Invalid operation!")
