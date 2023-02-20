357ed81f39f554c5bcbb2370f105c38ac15ff337
Skvortsov A.V <alexskvr03@gmail.com>
2023-02-20 23:25:59 +0300

    third subtask is done (creating list and filtering even number)

--- lab-1/main.py
+++ lab-1/main.py
@@ -4,14 +4,29 @@ from utilities import arithmetic_operation
 def main():
     print("Hello, World!")
     
-    first = int(input("Please enter first number: "))
-
-    second = int(input("Please enter second number: "))
-
-    operation = str(input("Please enter operation: "))
+    while True:
     
-    res = arithmetic_operation(first, second, operation)
-    print("Result is ", res)
+        first = int(input("Please enter first number: "))
+
+        second = int(input("Please enter second number: "))
+
+        operation = str(input("Please enter operation: "))
+    
+        res = arithmetic_operation(first, second, operation)
+        print("Result is ", res)
+
+        if str(input("Continue? (y/n)")) == "n":
+            break
+
+    list_of_numbers = []
+    try:
+        while True:
+            list_of_numbers.append(int(input("Enter list number (enter non-integer to stop): ")))
+    except:
+        print("Your list is ", list_of_numbers)
+        print("Even numbers of list are ", list(filter(lambda number: number % 2 == 0, list_of_numbers)))
+
+
 
     
 
