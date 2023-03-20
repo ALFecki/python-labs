5dfea7fd3c4a8e65aaa696d93b569ad24bba4b9b
ALFecki <alexskvr03@gmail.com>
2023-03-20 20:12:22 +0300

    task 1 is completed without input and file reading

--- lab-2/main.py
+++ lab-2/main.py
@@ -1,10 +1,19 @@
 from utilities import sentences_amount
-
-
+from utilities import non_declarative_sentecnes
+from utilities import split_to_words
+from utilities import average_length
+from utilities import average_words_len
+from utilities import words_top
 
 def main():
-    print("Hello, World")
-    print(sentences_amount("Do commanded an shameless we disposing do... Indulgence ten remarkably nor are impression out. Mr. Johnson provision an in intention. Saw supported too joy promotion engrossed propriety."))
+    data = "Do commanded an shameless we disposing do too. Indulgence ten remarkably nor are impression out. Mr. Johnson provision an in intention out. Saw supported too joy promotion engrossed propriety."
+    (sentences, count) = sentences_amount(data)
+    print("Sentences amount = ", count)
+    print("Non-declarative sentences amount = ", non_declarative_sentecnes(data))
+    print("Average len of sentence is ", average_length(sentences))
+    print("Average word len is ", average_words_len(sentences))
+    print("Top 10 of 3-grams: ", words_top(sentences, 10, 3))
+
 
 
 if __name__ == "__main__":
--- lab-2/utilities.py
+++ lab-2/utilities.py
@@ -1,7 +1,9 @@
 import re
+from collections import defaultdict
 
-pattern_sentences = r'(?<!Mr)[\.|!|?]+\"*\s*(?!$)'
+pattern_sentences = r'(?<!Mr)[\.|!|?]+[\"|\“|\”]*\s*(?!$)'
 pattern_non_declarative = r'(?<!Mr)[|!|?]+\"*\s*(?!$)'
+pattern_to_words = r"(\w+)"
 
 def sentences_amount(data: str):
     sentences = re.split(pattern_sentences, data)
@@ -14,7 +16,43 @@ def non_declarative_sentecnes(data: str):
     return amount
 
 
-# def average_length(sentences: list):
+def split_to_words(sentence: str):
+    words = re.findall(pattern_to_words, sentence)
+    for word in words:
+        if word.isdigit():
+            words.remove(word)
+    return words
     
-#     for sentence in sentences:
+
+def average_length(sentences: list):
+    (_, character_count) = words_and_char_amount(sentences)
+    return character_count / len(sentences)
+
+def words_and_char_amount(sentences: list):
+    character_count = 0
+    words_count = 0
+    for sentence in sentences:
+        words = split_to_words(sentence)
+        words_count += len(words)
+        character_count += sum([len(word) for word in words])
+    return (words_count, character_count)
+    
+
+def average_words_len(sentences: list):
+    (words_count, char_count) = words_and_char_amount(sentences)
+    return char_count / words_count
+
         
+def words_top(sentences: list, top_len = 10, word_len = 4):
+    top = defaultdict(int)
+    for sentence in sentences:
+        words = split_to_words(sentence)
+        for word in words:
+            if len(word) == word_len:
+                top[word] += 1
+    sorted_top = dict(sorted(top.items(),key=lambda item:item[1], reverse=True))
+    while len(sorted_top) > top_len:
+        sorted_top.pop()
+
+    return sorted_top
+
