6c45f657576a731a473436367c4edb4e937d50b9
ALFecki <alexskvr03@gmail.com>
2023-04-28 16:40:33 +0300

    json deserialization added

--- lab-3/serialization_tool/extension/serializators/json/utilities.py
+++ lab-3/serialization_tool/extension/serializators/json/utilities.py
@@ -1,5 +1,7 @@
 from frozendict import frozendict
-from constants import VALUE, TYPE
+from constants import VALUE, TYPE, ARRAY_IN_ARRAY_REGEX, ARRAY_REGEX, VALUE_REGEX
+import re
+
 
 def to_json(obj):
     ans = ""
@@ -31,4 +33,144 @@ def to_json(obj):
 
 
 def from_json(str):
-    pass
+    if str == "{}":
+        return frozendict()
+    elif str[0] == '{':
+        ans = dict()
+        string = str[1:len(str) - 1]
+        if re.match(ARRAY_IN_ARRAY_REGEX, string):
+            temp =""
+            flag = False
+            save_i = 0
+            ans_list = []
+            balance = 0
+            balance1 = 0
+            for i in range(8, len(string)):
+                if string[i] == '[' and not flag:
+                    balance1 += 1
+                elif string[i] == ']' and not flag:
+                    balance1 -= 1
+                if string[i] == '[' and not flag and balance1 <= 2:
+                    continue
+                elif string[i] == ']' and not flag and balance1 < 2:
+                    continue
+                elif string[i] == '{' and not flag:
+                    balance += 1
+                elif string[i] == '}' and not flag:
+                    balance -= 1
+                elif string[i] == '\"':
+                    flag = not flag
+                elif string[i] == ',' and not flag and balance == 0 and balance1 != 0:
+                    if temp != "" and temp != "[]":
+                        ans_list.append(from_json(temp))
+                    else:
+                        ans_list.append({})
+                    temp = ""
+                    continue
+                elif string[i] == ' ' and not flag and balance == 0:
+                    continue
+                elif string[i] == "," and not flag and balance1 == 0:
+                    if temp != "" and temp != "[]":
+                        ans_list.append(from_json(temp))
+                    else:
+                        ans_list.append({})
+                    save_i = i
+                    temp = ""
+                    break
+                temp += string[i]  
+            ans[VALUE] = {}
+
+            ans_list = tuple(ans_list)
+
+            for i in range(0, len(ans_list), 2):
+                ans[VALUE][ans_list[i]] = ans_list[i + 1]
+            temp = ""
+            for i in range(save_i + 11, len(string)):
+                if string[i] =='\"':
+                    ans[TYPE] = temp
+                    temp = ""
+                    break
+                else:
+                    temp += string[i]
+        elif re.match(ARRAY_REGEX, string):
+            temp = ""
+            flag = False
+            save_i = 0
+            ans_list = []
+            balance = 0
+            for i in range(10, len(string)):
+                if string[i] == '{' and not flag:
+                    balance += 1
+                elif string[i] == '}' and not flag:
+                    balance -= 1
+                if string[i] == '\"':
+                    flag = not flag
+                elif string[i] == ',' and not flag and balance == 0:
+                    ans_list.append(from_json(temp))
+                    temp = ""
+                    continue
+                elif string[i] == ' ' and not flag and balance == 0:
+                    continue
+                elif string[i] == "]" and not flag and balance == 0:
+                    if temp != "":
+                        ans_list.append(from_json(temp))
+                    save_i = i
+                    temp = ""
+                    break
+                temp += string[i]
+            ans_list = tuple(ans_list)
+            ans[VALUE] = ans_list
+
+            for i in range(save_i + 12, len(string)):
+                if string[i] == '\"':
+                    ans[TYPE] = temp
+                    temp = ""
+                    break
+                else:
+                    temp += string[i]
+
+        elif re.match(VALUE_REGEX, string):
+            temp = ""
+            flag = False
+            save_i = 0
+            balance = 0
+            for i in range(9, len(string)):
+                if string[i] == '{' and not flag:
+                    balance += 1
+                elif string[i] == '}' and not flag:
+                    balance -= 1
+                elif string[i] == '\"':
+                    flag = not flag
+                elif string[i] == "," and not flag and balance == 0:
+                    if temp != "":
+                        ans[VALUE] = from_json(temp)
+                    save_i = i
+                    temp = ""
+                    break
+                temp += string[i]
+            for i in range(save_i + 11, len(string)):
+                if string[i] == '\"':
+                    ans[TYPE] = temp
+                    temp = ""
+                    break
+                else:
+                    temp += string[i]
+
+        else:
+            temp = ""
+            flag = False
+            i = 10
+            while i < len(string):
+                if string[i] == '\"' and not flag:
+                    ans[TYPE] = temp
+                    temp = ""
+                    flag = True
+                    i += 11
+                elif string[i] == '\"' and flag:
+                    ans[TYPE] = temp
+                    temp = ""
+                    break
+                else:
+                    temp += string[i]
+                i += 1
+            return frozendict(ans)
